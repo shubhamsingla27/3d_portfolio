422f520fc76e4934cdcc9d757c876934961e966e
Shubham <shubhamcu27@gmail.com>
2023-08-11 19:21:41 -0700

    Testing changes for mobile

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -143,25 +143,61 @@ const ProjectCard = ({
 };
 
 const Works = () => {
+    const [isDesktop, setIsDesktop] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 768); // Adjust the breakpoint as needed
+        };
+
+        // Initial check
+        handleResize();
+
+        // Attach the event listener
+        window.addEventListener("resize", handleResize);
+
+        // Clean up the event listener on unmount
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
     return (
         <>
-            <motion.div variants={textVariant()}>
-                <p className={`${styles.sectionSubText} `}>My work</p>
-                <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-            </motion.div>
+            {isDesktop ? (
+                <motion.div variants={textVariant()}>
+                    <p className={`${styles.sectionSubText} `}>My work</p>
+                    <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+                </motion.div>
+            ) : (
+                <>
+                    <p className={`${styles.sectionSubText} `}>My work</p>
+                    <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+                </>
+            )}
 
             <div className="w-full flex">
-                <motion.p
-                    variants={fadeIn("", "", 0.1, 1)}
-                    className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-                >
-                    Following projects showcases my skills and experience
-                    through real-world examples of my work. Each project is
-                    briefly described with links to code repositories and live
-                    demos in it. It reflects my ability to solve complex
-                    problems, work with different technologies, and manage
-                    projects effectively.
-                </motion.p>
+                {isDesktop ? (
+                    <motion.p
+                        variants={fadeIn("", "", 0.1, 1)}
+                        className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+                    >
+                        Following projects showcases my skills and experience
+                        through real-world examples of my work. Each project is
+                        briefly described with links to code repositories and
+                        live demos in it. It reflects my ability to solve
+                        complex problems, work with different technologies, and
+                        manage projects effectively.
+                    </motion.p>
+                ) : (
+                    <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+                        Following projects showcases my skills and experience
+                        through real-world examples of my work. Each project is
+                        briefly described with links to code repositories and
+                        live demos in it. It reflects my ability to solve
+                        complex problems, work with different technologies, and
+                        manage projects effectively.
+                    </p>
+                )}
             </div>
 
             <div className="mt-20 flex flex-wrap gap-7">
